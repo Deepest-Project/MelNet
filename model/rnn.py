6685406eef26f7ded4a97b8d88b2e51e82abcffa
Seungwon Park <yyyyy@snu.ac.kr>
2019-09-03 14:08:29 +0900

    parallelize rnn ops

--- model/rnn.py
+++ model/rnn.py
@@ -14,10 +14,8 @@ class DelayedRNN(nn.Module):
 
         self.t_delay_RNN_x = nn.GRU(
             input_size=self.num_hidden, hidden_size=self.num_hidden, batch_first=True)
-        self.t_delay_RNN_y = nn.GRU(
-            input_size=self.num_hidden, hidden_size=self.num_hidden, batch_first=True)
-        self.t_delay_RNN_z = nn.GRU(
-            input_size=self.num_hidden, hidden_size=self.num_hidden, batch_first=True)
+        self.t_delay_RNN_yz = nn.GRU(
+            input_size=self.num_hidden, hidden_size=self.num_hidden, batch_first=True, bidirectional=True)
         self.c_RNN = nn.GRU(
             input_size=self.num_hidden, hidden_size=self.num_hidden, batch_first=True)
         self.f_delay_RNN = nn.GRU(
@@ -28,38 +26,37 @@ class DelayedRNN(nn.Module):
         self.W_f = nn.Linear(self.num_hidden, self.num_hidden)
 
     def forward(self, input_h_t, input_h_f, input_h_c):
-        # time-delayed stack 
-        h_t_x = torch.zeros(input_h_t.shape).cuda()
+        B, M, T, D = input_h_t.size()
 
-        for i in range(input_h_t.shape[2]): # line by line. TODO: parallelize
-            h_t_x_slice, _ = self.t_delay_RNN_x(input_h_t[:, :, i, :])
-            h_t_x[:, :, i, :] = h_t_x_slice
+        ####### time-delayed stack #######
+        # Fig. 2(a)-1 can be parallelized by viewing each horizontal line as batch
+        h_t_x, _ = self.t_delay_RNN_x(input_h_t.view(-1, T, D))
+        h_t_x = h_t_x.view(B, M, T, D)
 
-        h_t_y = torch.zeros(input_h_t.shape).cuda()
-        h_t_z = torch.zeros(input_h_t.shape).cuda()
-        for i in range(input_h_t.shape[1]): # line by line. TODO: parallelize
-            h_t_y_slice, _ = self.t_delay_RNN_y(input_h_t[:, i, :, :])
-            h_t_z_slice, _ = self.t_delay_RNN_z(input_h_t.flip(2)[:, i, :, :])
-            # TODO: can use bidirectional=True
-            h_t_y[:, i, :, :] = h_t_y_slice
-            h_t_z[:, i, :, :] = h_t_z_slice.flip(1)
+        # Fig. 2(a)-2,3 can be parallelized by viewing each vertical line as batch,
+        # using bi-directional version of GRU
+        temp = input_h_t.transpose(1, 2) # [B, T, M, D]
+        temp = temp.view(-1, M, D)
+        h_t_yz, _ = self.t_delay_RNN_yz(temp)
+        h_t_yz = h_t_yz.view(B, T, M, D)
+        h_t_yz = h_t_yz.transpose(1, 2)
 
-        h_t_concat = torch.cat((h_t_x, h_t_y, h_t_z), dim=3)
+        h_t_concat = torch.cat((h_t_x, h_t_yz), dim=3)
         output_h_t = input_h_t + self.W_t(h_t_concat) # residual connection, eq. (6)
 
-        # centralized stack
+        ####### centralized stack #######
         h_c_temp, _ = self.c_RNN(input_h_c)
         output_h_c = input_h_c + self.W_c(h_c_temp) # residual connection, eq. (11)
 
-        # frequency-delayed stack
+        ####### frequency-delayed stack #######
         h_c_expanded = output_h_c.unsqueeze(1).repeat(1, self.freq, 1, 1)
         h_f_sum = input_h_f + output_h_t + h_c_expanded
+        h_f_sum = h_f_sum.transpose(1, 2) # [B, T, M, D]
+        h_f_sum = h_f_sum.view(-1, M, D)
 
-        h_f_temp = torch.zeros(input_h_f.shape).cuda()
-        for i in range(h_f_sum.shape[1]):
-            h_f_slice, _ = self.f_delay_RNN(h_f_sum[:, i, :, :])
-            h_f_temp[:, i, :, :] = h_f_slice
-
+        h_f_temp, _ = self.f_delay_RNN(h_f_sum)
+        h_f_temp = h_f_temp.view(B, T, M, D)
+        h_f_temp = h_f_temp.transpose(1, 2) # [B, M, T, D]
         output_h_f = input_h_f + self.W_f(h_f_temp) # residual connection, eq. (8)
 
         return output_h_t, output_h_f, output_h_c
