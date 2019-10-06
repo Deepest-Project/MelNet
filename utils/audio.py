cc3ac7a49111d4562a35f1975df99de454a663eb
Rick-McCoy <rickmccoy3141@gmail.com>
2019-10-06 23:12:52 +0900

    Removed wrongly placed .cuda()s in MelGen

--- utils/audio.py
+++ utils/audio.py
@@ -8,11 +8,11 @@ import numpy as np
 class MelGen():
     def __init__(self, hp):
         self.hp = hp
-        self.window = torch.hann_window(window_length=hp.audio.win_length).cuda()
+        self.window = torch.hann_window(window_length=hp.audio.win_length)
         self.mel_basis = librosa.filters.mel(
             sr=hp.audio.sr, n_fft=hp.audio.n_fft, n_mels=hp.audio.n_mels)
         self.mel_basis = \
-            torch.from_numpy(self.mel_basis).cuda().float() # [n_mels, n_fft//2+1]
+            torch.from_numpy(self.mel_basis).float() # [n_mels, n_fft//2+1]
 
     def get_magnitude(self, x):
         x = torch.stft(x,
