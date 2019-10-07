ac172884870b89866d9aaf2f613d64c126e7be7b
Rick-McCoy <rickmccoy3141@gmail.com>
2019-10-07 21:40:38 +0900

    Add gmm.py

---
+++ utils/gmm.py
@@ -0,0 +1,13 @@
+import torch
+
+def get_pi_indices(pi):
+    cumsum = torch.cumsum(pi, dim=3)
+    rand = torch.rand(pi.shape[:-1])
+    indices = pi.shape[-1] - (cumsum < rand).sum(dim=-1)
+    return indices
+
+def sample_gmm(mu, std, pi):
+    indices = get_pi_indices(pi)
+    mu = mu[indices]
+    std = std[indices]
+    return torch.normal(mu, std)
