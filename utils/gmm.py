129e3a0fcca0772c1350dcea9ceb9724ffa33c5c
Rick-McCoy <rickmccoy3141@gmail.com>
2019-10-07 23:33:30 +0900

    Fix bug in sample_gmm

--- utils/gmm.py
+++ utils/gmm.py
@@ -4,10 +4,10 @@ def get_pi_indices(pi):
     cumsum = torch.cumsum(pi.cpu(), dim=3)
     rand = torch.rand(pi.shape[:-1] + (1,))
     indices = pi.shape[-1] - (cumsum < rand).sum(dim=3)
-    return indices.detach().numpy()
+    return indices.flatten().detach().numpy()
 
 def sample_gmm(mu, std, pi):
     indices = get_pi_indices(pi)
-    mu = mu[indices]
-    std = std[indices]
+    mu = mu.reshape(-1, mu.shape[-1])[indices].reshape_as(std)
+    std = std.reshape(-1, std.shape[-1])[indices].reshape_as(mu)
     return torch.normal(mu, std).reshape_as(mu)
