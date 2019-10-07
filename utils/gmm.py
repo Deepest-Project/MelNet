Fixed broadcasting bug

utils/gmm.py
@@ -2,8 +2,8 @@ import torch
 
 def get_pi_indices(pi):
     cumsum = torch.cumsum(pi, dim=3)
-    rand = torch.rand(pi.shape[:-1])
-    indices = pi.shape[-1] - (cumsum < rand).sum(dim=-1)
+    rand = torch.rand(pi.shape[:-1] + (1,))
+    indices = pi.shape[-1] - (cumsum < rand).sum(dim=3)
     return indices
 
 def sample_gmm(mu, std, pi):