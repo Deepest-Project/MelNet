a34b1330bb2647344f7c3b90d3410c7da5c6ff1f
Rick-McCoy <rickmccoy3141@gmail.com>
2019-10-07 22:47:08 +0900

    More bug fixing: .cuda() in get_pi_indices

--- utils/gmm.py
+++ utils/gmm.py
@@ -2,7 +2,7 @@ import torch
 
 def get_pi_indices(pi):
     cumsum = torch.cumsum(pi, dim=3)
-    rand = torch.rand(pi.shape[:-1] + (1,))
+    rand = torch.rand(pi.shape[:-1] + (1,)).cuda()
     indices = pi.shape[-1] - (cumsum < rand).sum(dim=3)
     return indices
 
