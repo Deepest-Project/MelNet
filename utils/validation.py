5ae674a63e02885273963012c2f422fa7dd13af9
Rick-McCoy <rickmccoy3141@gmail.com>
2019-10-21 12:00:41 +0900

    Changed test loss calculation

--- utils/validation.py
+++ utils/validation.py
@@ -10,17 +10,15 @@ def validate(args, model, melgen, tierutil, testloader, criterion, writer, step)
     model.eval()
     torch.backends.cudnn.benchmark = False
 
-    test_loss = 0.0
+    test_loss = []
     loader = tqdm.tqdm(testloader, desc='Testing is in progress')
     with torch.no_grad():
         for source, target in loader:
             mu, std, pi = model(source.cuda())
             loss = criterion(target.cuda(), mu, std, pi)
+            test_loss.append(loss.item())
 
-            loss = loss.item()
-            test_loss += loss
-
-        test_loss /= len(testloader.dataset)
+        test_loss = sum(test_loss) / len(test_loss)
         source = source[0].cpu().detach().numpy()
         target = target[0].cpu().detach().numpy()
         result = sample_gmm(mu[0], std[0], pi[0]).cpu().detach().numpy()
